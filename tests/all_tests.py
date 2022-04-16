55bf4dab19fb37b16260f7cd859c1fad7815b437
dots-git <simon@kriele.de>
2022-04-16 23:56:31 +0200

    modified unit tests

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -4,6 +4,8 @@ all_tests = []
 from src.thelittlethings import multi_test, Log
 def run_tests():
     multi_test(*all_tests)
+    Log.print = False
+    Log("\nuse the search function (ctrl+f) to find failed tests and summaries")
 import atexit
 
 atexit.register(run_tests)
