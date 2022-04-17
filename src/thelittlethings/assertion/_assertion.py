eef9174532203350db05bb12e45329d572108e85
dots-git <simon@kriele.de>
2022-04-17 23:53:30 +0200

    fixed error in assertions

--- src/thelittlethings/assertion/_assertion.py
+++ src/thelittlethings/assertion/_assertion.py
@@ -102,13 +102,13 @@ def assert_close(a, b, epsilon=1e-6, error_message_appendix=None):
         raise AssertionError(f"{a} != {b}{error_message_appendix}")
 
 def assert_type(obj, type, error_message_appendix=None):
-    if not isinstance(obj, type, error_message_appendix=None):
-        if isinstance(type, Iterable, error_message_appendix=None):
+    if not isinstance(obj, type):
+        if isinstance(type, Iterable):
             if len(type) > 0:
                 type_str = type[-1].__name__
             if len(type) > 1:
                 type_str = type[-2].__name__ + " or " + type_str
-            for i in range(3, len(type) + 1, error_message_appendix=None):
+            for i in range(3, len(type) + 1):
                 type_str = type[-i].__name__ + ", " + type_str
         else:
             type_str = type.__name__
@@ -121,16 +121,16 @@ def assert_type(obj, type, error_message_appendix=None):
     return True
 
 def assert_types(objects, types, error_message_appendix=None):
-    for index, obj, type in enumerate(zip(objects, types), error_message_appendix=None):
+    for index, obj, type in enumerate(zip(objects, types)):
         try:
             assert_type(obj, type)
         except AssertionError as e:
-            if isinstance(type, Iterable, error_message_appendix=None):
+            if isinstance(type, Iterable):
                 if len(type) > 0:
                     type_str = type[-1].__name__
                 if len(type) > 1:
                     type_str = type[-2].__name__ + " or " + type_str
-                for i in range(3, len(type) + 1, error_message_appendix=None):
+                for i in range(3, len(type) + 1):
                     type_str = type[-i].__name__ + ", " + type_str
             else:
                 type_str = type.__name__
